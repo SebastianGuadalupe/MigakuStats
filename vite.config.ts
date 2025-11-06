4728b862f695e2d8652991e7df04244930a86f6d
Sebastián Guadalupe <sebastianguadalupe00@gmail.com>
2025-11-06 17:17:12 -0300

    Fix intervals overlapping and not showing properly

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         icon: 'https://study.migaku.com/favicon.ico',
         namespace: 'http://tampermonkey.net/',
         match: ['https://study.migaku.com/*'],
-        version: '0.2.2',
+        version: '0.2.3',
         description: 'More stats for Migaku Memory.',
         author: 'sguadalupe',
         license: 'GPL-3.0',
